39da60100526c24fa373b9ea191349ed9feefaa4
xnuke2 <xmnikit@gmail.com>
2025-02-19 06:18:46 +0300

    Добавьте файлы проекта.

---
+++ lab1_MathProg/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace lab1_MathProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownLiberty = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownBase = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewConditions = new System.Windows.Forms.DataGridView();
+            this.buttonCreatetableToConditions = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonCulculate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewRezult = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewAnswer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLiberty)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewConditions)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRezult)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAnswer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownLiberty
+            // 
+            this.numericUpDownLiberty.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownLiberty.Location = new System.Drawing.Point(309, 34);
+            this.numericUpDownLiberty.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownLiberty.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownLiberty.Name = "numericUpDownLiberty";
+            this.numericUpDownLiberty.Size = new System.Drawing.Size(136, 22);
+            this.numericUpDownLiberty.TabIndex = 0;
+            this.numericUpDownLiberty.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownBase
+            // 
+            this.numericUpDownBase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownBase.Location = new System.Drawing.Point(309, 69);
+            this.numericUpDownBase.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownBase.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownBase.Name = "numericUpDownBase";
+            this.numericUpDownBase.Size = new System.Drawing.Size(136, 22);
+            this.numericUpDownBase.TabIndex = 1;
+            this.numericUpDownBase.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество свободных переменных";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Количество базовых переменных";
+            // 
+            // dataGridViewConditions
+            // 
+            this.dataGridViewConditions.AllowUserToAddRows = false;
+            this.dataGridViewConditions.AllowUserToDeleteRows = false;
+            this.dataGridViewConditions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewConditions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridViewConditions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewConditions.Location = new System.Drawing.Point(8, 101);
+            this.dataGridViewConditions.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewConditions.Name = "dataGridViewConditions";
+            this.dataGridViewConditions.RowHeadersVisible = false;
+            this.dataGridViewConditions.RowHeadersWidth = 51;
+            this.dataGridViewConditions.Size = new System.Drawing.Size(753, 212);
+            this.dataGridViewConditions.TabIndex = 4;
+            // 
+            // buttonCreatetableToConditions
+            // 
+            this.buttonCreatetableToConditions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCreatetableToConditions.Location = new System.Drawing.Point(495, 32);
+            this.buttonCreatetableToConditions.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCreatetableToConditions.Name = "buttonCreatetableToConditions";
+            this.buttonCreatetableToConditions.Size = new System.Drawing.Size(267, 62);
+            this.buttonCreatetableToConditions.TabIndex = 5;
+            this.buttonCreatetableToConditions.Text = "Создать таблицу для задания условий";
+            this.buttonCreatetableToConditions.UseVisualStyleBackColor = true;
+            this.buttonCreatetableToConditions.Click += new System.EventHandler(this.buttonCreatetableToConditions_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonCulculate);
+            this.groupBox1.Controls.Add(this.buttonCreatetableToConditions);
+            this.groupBox1.Controls.Add(this.dataGridViewConditions);
+            this.groupBox1.Controls.Add(this.numericUpDownLiberty);
+            this.groupBox1.Controls.Add(this.numericUpDownBase);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(20, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(769, 357);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Задание условий";
+            // 
+            // buttonCulculate
+            // 
+            this.buttonCulculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCulculate.Location = new System.Drawing.Point(601, 320);
+            this.buttonCulculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCulculate.Name = "buttonCulculate";
+            this.buttonCulculate.Size = new System.Drawing.Size(160, 26);
+            this.buttonCulculate.TabIndex = 7;
+            this.buttonCulculate.Text = "Вычислить";
+            this.buttonCulculate.UseVisualStyleBackColor = true;
+            this.buttonCulculate.Click += new System.EventHandler(this.buttonCulculate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.dataGridViewRezult);
+            this.groupBox2.Location = new System.Drawing.Point(20, 379);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(769, 358);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Решение";
+            // 
+            // dataGridViewRezult
+            // 
+            this.dataGridViewRezult.AllowUserToAddRows = false;
+            this.dataGridViewRezult.AllowUserToDeleteRows = false;
+            this.dataGridViewRezult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewRezult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewRezult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRezult.Location = new System.Drawing.Point(8, 23);
+            this.dataGridViewRezult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewRezult.Name = "dataGridViewRezult";
+            this.dataGridViewRezult.RowHeadersVisible = false;
+            this.dataGridViewRezult.RowHeadersWidth = 51;
+            this.dataGridViewRezult.Size = new System.Drawing.Size(753, 327);
+            this.dataGridViewRezult.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.dataGridViewAnswer);
+            this.groupBox3.Location = new System.Drawing.Point(20, 735);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(769, 122);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ответ";
+            // 
+            // dataGridViewAnswer
+            // 
+            this.dataGridViewAnswer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewAnswer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAnswer.Location = new System.Drawing.Point(8, 23);
+            this.dataGridViewAnswer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewAnswer.Name = "dataGridViewAnswer";
+            this.dataGridViewAnswer.RowHeadersWidth = 51;
+            this.dataGridViewAnswer.Size = new System.Drawing.Size(753, 91);
+            this.dataGridViewAnswer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 871);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLiberty)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewConditions)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRezult)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAnswer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownLiberty;
+        private System.Windows.Forms.NumericUpDown numericUpDownBase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewConditions;
+        private System.Windows.Forms.Button buttonCreatetableToConditions;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonCulculate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridViewRezult;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridViewAnswer;
+    }
+}
+
---
+++ lab1_MathProg/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_MathProg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreatetableToConditions_Click(object sender, EventArgs e)
+        {
+            dataGridViewConditions.Rows.Clear();
+            dataGridViewConditions.Columns.Clear();
+            int baseX =Convert.ToInt32(numericUpDownBase.Value);
+            int libertyX =Convert.ToInt32(numericUpDownLiberty.Value);
+            dataGridViewConditions.Columns.Add("Xk" , "Xk" );
+            for (int i = 0; i < baseX + libertyX ; i++)
+            {
+                dataGridViewConditions.Columns.Add("x"+(i+1),"x"+(i+1));
+            }
+            dataGridViewConditions.Columns.Add("b", "b" );
+            for (int i = libertyX; i < baseX + libertyX; i++)
+            {
+                dataGridViewConditions.Rows.Add("x"+(i+1));
+            }
+            dataGridViewConditions.Rows.Add("Z");
+            for (int i = 0;i < baseX + 1; i++)
+                for (int j = 1;j < baseX + libertyX+2; j++)
+                {
+                    dataGridViewConditions.Rows[i].Cells[j].Value = 0;
+                }
+            
+        }
+
+        private void buttonCulculate_Click(object sender, EventArgs e)
+        {
+            dataGridViewRezult.Rows.Clear();
+            dataGridViewRezult.Columns.Clear();
+            
+            
+            int columns = Convert.ToInt32(dataGridViewConditions.Columns.Count) - 1;
+            int rows = Convert.ToInt32(dataGridViewConditions.Rows.Count);
+
+                dataGridViewRezult.Columns.Add("Xk", "Xk");
+            for (int i = 0; i < columns - 1; i++)
+            {
+                dataGridViewRezult.Columns.Add("x" + (i + 1), "x" + (i + 1));
+            }
+            dataGridViewRezult.Columns.Add("b", "b");
+            dataGridViewRezult.Columns.Add("s", "s");
+            
+            int[] baseNames = new int[rows];
+            for (int i = 0; i < rows-1; i++)
+            {
+                baseNames[i] = columns -rows+i;
+            }
+            double[,] matrix = new double[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = Convert.ToDouble(Convert.ToString( dataGridViewConditions[j+1,i].Value).Replace(",","."));
+                }
+            }
+            int ch = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i, columns-1] < 0)
+                {
+                    dataGridViewAnswer.Rows.Clear();
+                    dataGridViewAnswer.Columns.Clear();
+                    dataGridViewAnswer.Columns.Add("b", "Ошибка");
+                    dataGridViewAnswer.Rows.Add("Решение отсутствует");
+                    return;
+                };
+            }
+
+
+            ch = 0;
+            for(int i = 0; i < columns; i++)
+            {
+                for(int j = 0; j < rows; j++)
+                    if (matrix[j, i] == 1)
+                    {
+                        ch++;
+                        for (int k = 0; k < rows; k++)
+                        {
+                            if (matrix[k, i] != 0&&k!=j)
+                            {
+                                ch--;
+                                break; 
+                            }
+                        } 
+                    }  
+
+            }
+            if (ch != baseNames.Length-1) Mmetod();
+            else simplex(columns, rows, baseNames, matrix);
+
+        }
+        private void simplex(int columns, int rows, int[] baseNames, double[,] matrix)
+        {
+            int rowIndGL = 0;
+            DataGridViewCellStyle styleGreen = new DataGridViewCellStyle();
+            styleGreen.BackColor = Color.Green;
+            styleGreen.ForeColor = Color.Black;
+            //row.Cells[color.Index].Style = style;
+            DataGridViewCellStyle styleRed = new DataGridViewCellStyle();
+            styleRed.BackColor = Color.Red;
+            styleRed.ForeColor = Color.Black;
+            int indSimRel = 0;
+            while (true)
+            {
+
+                double min = 0;
+                int indMin = 0;
+                for (int i = 0; i < columns; i++)
+                {
+                    if (matrix[rows - 1, i] < min)
+                    {
+                        min = matrix[rows - 1, i];
+                        indMin = i;
+                    }
+                }
+                if (min >= 0)
+                {
+                    dataGridViewAnswer.Rows.Clear();
+                    dataGridViewAnswer.Columns.Clear();
+                    //сохранение итерации
+                    for (int i = 0; i < rows - 1; i++)
+                    {
+                        dataGridViewRezult.Rows.Add("x" + (baseNames[i] + 1));
+
+                    }
+                    dataGridViewRezult.Rows.Add("Z");
+
+
+                    for (int i = 0; i < rows; i++)
+                        for (int j = 0; j < columns; j++)
+                        {
+                            dataGridViewRezult[+j + 1, rowIndGL + i].Value = matrix[i, j];
+                        }
+
+                    dataGridViewRezult.Rows.Add();
+                    rowIndGL += rows + 1;
+
+
+                    for (int i = 0; i < rows-1; i++)
+                    {
+                        dataGridViewAnswer.Columns.Add("x" + (i + 1), "x" + (i + 1));
+                    }
+                    dataGridViewAnswer.Columns.Add("b", "Z");
+                    dataGridViewAnswer.Rows.Add();
+                    for (int i = 0; i < rows - 1; i++)
+                        dataGridViewAnswer[i, 0].Value = 0;
+
+                    for (int i = 0; i < rows - 1; i++)
+                    {
+                        if (baseNames[i] > rows - 1) continue;
+                        else dataGridViewAnswer[i, 0].Value = matrix[i, columns - 1];
+                    }
+                    dataGridViewAnswer[rows - 1, 0].Value = matrix[rows - 1, columns - 1];
+                    dataGridViewAnswer.Rows.Add();
+
+                    for (int j = 0; j < rows-1; j++)
+                    {
+                        for (int k = 0; k < rows - 1; k++)
+                            if (baseNames[k]== j + rows-1)
+                            {
+                                dataGridViewAnswer[j, 1].Value = "недефицитный";
+                                break;
+                            }
+                         if(Convert.ToString(dataGridViewAnswer[j, 1].Value)=="")       
+                                dataGridViewAnswer[j , 1].Value = "дефицитный (ценность - " + matrix[rows - 1, j + rows-1] + ")";
+                    }
+                    return;
+                }
+                double[] simRel = new double[rows];
+                bool find = false;
+                for (int i = 0; i < rows; i++)
+                    if (matrix[i, indMin] > 0)
+                    {
+                        simRel[i] = matrix[i, columns - 1] / matrix[i, indMin];
+                        find = true;
+                    }
+
+
+                if (!find)
+                {
+                    dataGridViewAnswer.Rows.Clear();
+                    dataGridViewAnswer.Columns.Clear();
+                    dataGridViewAnswer.Columns.Add("b", "Ошибка");
+                    dataGridViewAnswer.Rows.Add("Оптимальное решение отсутствует");
+                    //оптимальное отсутсвуект
+                    return;
+                }
+                min = int.MaxValue;
+                indSimRel = 0;
+                for (int i = 0; i < rows; i++)
+                    if (simRel[i] < min && simRel[i] != 0)
+                    {
+                        min = simRel[i];
+                        indSimRel = i;
+                    }
+                //сохранение итерации
+                for (int i = 0; i < rows - 1; i++)
+                {
+                    dataGridViewRezult.Rows.Add("x" + (baseNames[i] + 1));
+                    dataGridViewRezult.Rows[rowIndGL + i].Cells[columns + 1].Value = simRel[i];
+                }
+                dataGridViewRezult.Rows.Add("Z");
+                dataGridViewRezult.Rows[rows - 1].Cells[columns + 1].Value = simRel[rows - 1];
+
+                for (int i = 0; i < rows; i++)
+                    for (int j = 0; j < columns; j++)
+                    {
+                        dataGridViewRezult[+j + 1, rowIndGL + i].Value = matrix[i, j];
+                    }
+
+                for (int i = 0; i < rows; i++)
+                    dataGridViewRezult[indMin + 1, rowIndGL + i].Style = styleGreen;
+                for (int j = 0; j < columns + 1; j++)
+                    dataGridViewRezult[j + 1, rowIndGL + indSimRel].Style = styleRed;
+                dataGridViewRezult.Rows.Add();
+                rowIndGL += rows + 1;
+
+                //переход к следующему решению
+                baseNames[indSimRel] = indMin;
+                double tmp = matrix[indSimRel, indMin];
+                for (int i = 0; i < columns; i++)
+                {
+                    matrix[indSimRel, i] = matrix[indSimRel, i] / tmp;
+                }
+                double[] tmp2 = new double[rows];
+                for (int i = 0; i < rows; i++)
+                    tmp2[i] = matrix[i, indMin];
+                for (int i = 0; i < rows; i++)
+                {
+                    if (i == indSimRel) continue;
+                    for (int j = 0; j < columns; j++)
+                    {
+                        matrix[i, j] = matrix[i, j] + matrix[indSimRel, j] * (-tmp2[i]);
+                    }
+                }
+
+            }
+        }
+        private void Mmetod()
+        {
+
+        }
+    }
+}
